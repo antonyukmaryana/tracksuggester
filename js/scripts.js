ae650ad340cc51b5c6e28ff248354b3e351aab23
Maryana <amaryana@gmail.com>
2019-03-29 16:54:45 -0700

    add fixed html

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
-$().ready(function(){
-  $("form#trackspectrum").submit(function(event){
-    var answer1 = parseInt($("select#question1").val());
-    var answer2 = parseInt($("select#question2").val());
-    var answer3 = parseInt($("select#question3").val());
-    var answer3 = parseInt($("select#question4").val());
-    var answer5 = parseInt($("select#question5").val());
-    var total = answer1 + answer2 + answer3 + number4 + number5
+$(document).ready(function () {
+    console.log("Hello");
 
-    if (total < 0) {
-      result = "C#/.Net";
-    } else if (total > 0) {
-      result = "CSS/Design";
-    } else {
-      result = "Java/Android";
-    }
+    $("form#trackspectrum").submit(function () {
+        var answer1 = parseInt($("select#question1").val());
+        var answer2 = parseInt($("select#question2").val());
+        var answer3 = parseInt($("select#question3").val());
+        var answer4 = parseInt($("select#question4").val());
+        var answer5 = parseInt($("select#question5").val());
+        var total = answer1 + answer2 + answer3 + answer4 + answer5
 
-    $("#track").empty().append(result);
-    $("#result").show();
+        if (total < 0) {
+            result = "C#/.Net";
+        } else if (total > 0) {
+            result = "CSS/Design";
+        } else {
+            result = "Java/Android";
+        }
 
-      event.preventDefault();
+        $("#track").empty().append(result);
+        $("#result").show();
+
+        event.preventDefault();
     });
-  });
+});
